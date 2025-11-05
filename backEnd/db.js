16f77d168e2340e060a51a885c2ff59bf24f4ed0
sneha00726 <151377633+sneha00726@users.noreply.github.com>
2025-11-05 11:52:12 +0530

    update db conncetion,.env changes and deployemet configuration

--- backEnd/db.js
+++ backEnd/db.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 let mysql=require("mysql2");
 
 let conn=mysql.createConnection({
-    host:process.env.db_host,
+     host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  multipleStatements: true
+  //local database 
+   /* host:process.env.db_host,
     user:process.env.db_username,
     password:process.env.db_password,
     database:process.env.db_name,
-    multipleStatements: true  
+    multipleStatements: true  */
 });
 
 conn.connect((err)=>
